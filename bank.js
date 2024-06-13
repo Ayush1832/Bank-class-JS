d03fbb395eb82431c306c12590c94a4e1cd4fb86
Ayush Nayak <nayaksubham18@gmail.com>
2024-06-13 18:36:15 +0530

    update bank.js -withdraw

--- bank.js
+++ bank.js
@@ -15,3 +15,17 @@ class BankAccount {
         this.transactionHistory.push({ type: 'Deposit', amount, date: new Date() });
         this.updateAccountDetails();
     }
+
+    withdraw(amount) {
+        if (amount <= 0) {
+            alert('Withdrawal amount must be positive.');
+            return;
+        }
+        if (amount > this.balance) {
+            alert('Insufficient funds.');
+            return;
+        }
+        this.balance -= amount;
+        this.transactionHistory.push({ type: 'Withdraw', amount, date: new Date() });
+        this.updateAccountDetails();
+    }
